ec01c88da48e90ee47bae69e89f313921ba9f86a
ismail çelik <ismailceliktr00@gmail.com>
2020-01-18 21:44:41 +0300

    ok

---
+++ src/components/User.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Test1</li>
+                    <li>Test2</li>
+                    <li>Test3</li>
+                    <li>Test4</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default User;
